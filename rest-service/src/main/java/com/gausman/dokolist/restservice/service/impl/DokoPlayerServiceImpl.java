2ac1d5dd8d35d904807cb7723c8e904a8a7c75e8
Erik Jansen <ejms116@gmail.com>
2024-10-11 15:45:33 +0200

    initial commit

--- rest-service/src/main/java/com/gausman/dokolist/restservice/service/impl/DokoPlayerServiceImpl.java
+++ rest-service/src/main/java/com/gausman/dokolist/restservice/service/impl/DokoPlayerServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class DokoDokoPlayerServiceImpl implements DokoPlayerService {
+public class DokoPlayerServiceImpl implements DokoPlayerService {
     @Autowired
     DokoPlayerRepository dokoPlayerRepository;
 
