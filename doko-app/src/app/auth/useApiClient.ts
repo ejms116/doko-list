10e4400f83e96455412e5feeb39afc77290adff4
Erik Jansen <ejms116@gmail.com>
2024-12-19 22:05:01 +0100

    url fix 3

--- doko-app/src/app/auth/useApiClient.ts
+++ doko-app/src/app/auth/useApiClient.ts
@@ -6,7 +6,7 @@ const useApiClient = () => {
   const { authToken } = useContext(AuthContext);
 
   const apiClient = axios.create({
-    baseURL: '',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   });
 
   // Add interceptors to attach headers
