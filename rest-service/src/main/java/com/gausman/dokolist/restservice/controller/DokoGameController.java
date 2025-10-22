b3e1c46f8db2c715ae04a0bf44580d444f076042
Erik Jansen <ejms116@gmail.com>
2025-10-22 18:44:15 +0200

    session delete use request instead of delete

--- rest-service/src/main/java/com/gausman/dokolist/restservice/controller/DokoGameController.java
+++ rest-service/src/main/java/com/gausman/dokolist/restservice/controller/DokoGameController.java
@@ -108,7 +108,7 @@ public class DokoGameController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @DeleteMapping("/games/{gameId}")
+    @RequestMapping("/games/{gameId}")
     public ResponseEntity<?> delete(@PathVariable Long gameId, HttpServletRequest httpServletRequest){
         Optional<DokoGame> dokoGame = dokoGameService.findById(gameId);
 
